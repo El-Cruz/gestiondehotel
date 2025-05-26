443e508911b845bc4cb983938012c4be1e548ac7
Nico <nicocruzur04@gmail.com>
2025-05-25 19:01:14 -0500

    Asignacion de roles

--- Hotel.java
+++ Hotel.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private String nombre;
+    private ArrayList<Habitacion> habitaciones;
+    private ArrayList<Reserva> reservas;
+
+    public Hotel(String nombre) {
+        this.nombre = nombre;
+        this.habitaciones = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+    }
+
+    public void agregarHabitacion(Habitacion habitacion) {
+        habitaciones.add(habitacion);
+    }
+
+    public boolean hayDisponibilidad(int numeroHabitacion) {
+        for (Habitacion h : habitaciones) {
+            if (h.getNumero() == numeroHabitacion) {
+                return h.isDisponible();
+            }
+        }
+        return false;
+    }
+
+    public void crearReserva(String cliente, int numeroHabitacion, LocalDate entrada, LocalDate salida) {
+        for (Habitacion h : habitaciones) {
+            if (h.getNumero() == numeroHabitacion && h.isDisponible()) {
+                Reserva r = new Reserva(cliente, h, entrada, salida);
+                reservas.add(r);
+                break;
+            }
+        }
+    }
+
+    public ArrayList<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public ArrayList<Reserva> getReservas() {
+        return reservas;
+    }
 }
+
